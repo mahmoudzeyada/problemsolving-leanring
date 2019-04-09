7b2452bd8a1f9a119df613c7268363d86de92e2b
mahmoudzeyada <mahmoudzeyada440@gmail.com>
2019-04-09 18:04:43 +0200

    learing fqin on line coding bython

--- js/encryption.py
+++ js/encryption.py
@@ -92,12 +92,11 @@
 # print(encryption(s))
 # (lambda s: (lambda r: print(' '.join(map(lambda x: s[x::r], range(r)))))(int(-(-(len(s)**0.5)//1))))(input().strip())
 # import sys
-# from math import ceil, sqrt
+from math import ceil, sqrt
 
 
-# s = input().strip()
-# c = ceil(sqrt(len(s)))
-# print(' '.join(map(lambda x: s[x::c], range(c))))
-
-(lambda s: ((lambda c: print(" ".join(map(lambda x: s[x::4], range(c)))))(
-    int(-(-(len(s)**0.5)//1)))))(input().strip())
+(lambda s: (lambda c: (print(' '.join(map(lambda x: s[x::c], range(c))))))(
+    ceil(sqrt(len(s)))))(input().strip())
+# this is fqin one line code
+# (lambda s: ((lambda c: print(" ".join(map(lambda x: s[x::4], range(c)))))(
+# int(-(-(len(s)**0.5)//1)))))(input().strip())
